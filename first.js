0590df81ccd4f20362484b034bc38bd02b57d037
Educative-Anupam <148734367+Educative-Anupam@users.noreply.github.com>
2024-05-24 22:16:16 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,16 @@
+let loginBtn=document.querySelector('.btnn');
+loginBtn.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("You are successfully Logged in.");
+
+}
+
+
+
+
+
+
+
+
+
